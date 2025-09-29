5601004a6c0890ab83b392db782f1d406a59954e
TamanaMajedi-CS <tamanamajede@gmail.com>
2025-09-29 14:17:21 +0430

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; 
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
@@ -8,12 +8,12 @@ import { FavoritesProvider } from "./context/FavoritesContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <FavoritesProvider>
           <App />
         </FavoritesProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
